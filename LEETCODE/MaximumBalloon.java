d5e7107eef74726f1546617b0b1180861c186f7a
ISHRAQ01 <ishraq0641@gmail.com>
2026-06-25 20:20:45 +0530

    1189 problem

--- LEETCODE/MaximumBalloon.java
+++ LEETCODE/MaximumBalloon.java
@@ -1,10 +1,21 @@
+
 public class MaximumBalloon{
 
     public static int MaxBalloon(String text){
-        int[] charFreq
+        int[] charFreq=new int[26];
+        for (char c:text.toCharArray()){
+            charFreq[c-'a']++;
+        }
+        int b=charFreq['b'-'a'];
+        int a=charFreq['a'-'a'];
+        int l=charFreq['l'-'a']/2;
+        int o=charFreq['o'-'a']/2;
+        int n=charFreq['n'-'a'];
+
+        return Math.min(Math.min(b,a),Math.min(Math.min(o,l),n));
     }
     public static void main(String args[]){
-        String text="balloon";
-        MaxBalloon(text);
+        String text="balon";
+        System.out.println(MaxBalloon(text));
     }
 }
