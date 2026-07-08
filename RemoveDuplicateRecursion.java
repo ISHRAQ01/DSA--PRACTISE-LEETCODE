1822234ecf77d39a2db6ba71dc58be6d6869e648
ISHRAQ01 <ishraq0641@gmail.com>
2026-07-08 16:07:58 +0530

    Duplicate string recursion

---
+++ RemoveDuplicateRecursion.java
@@ -0,0 +1,21 @@
+public class RemoveDuplicateRecursion {
+
+    public static void removeDuplicate(String str,int i,StringBuilder sb,boolean[] map){
+        if(i==str.length()){
+            System.out.println(sb);
+            return;
+        }
+        if(map[str.charAt(i)-'a']==true){
+            removeDuplicate(str, i+1, sb, map);
+        }
+        else{
+            map[str.charAt(i)-'a']=true;
+            removeDuplicate(str, i+1, sb.append(str.charAt(i)), map);
+        }
+
+    }
+    public static void main(String[] args) {
+     String str="apnacollege";
+     removeDuplicate(str,0,new StringBuilder(""),new boolean[26]);
+    }
+}
